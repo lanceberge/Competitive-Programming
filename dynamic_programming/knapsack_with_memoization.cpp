f1ad2bd23f37140caeea29adeef23f25e60bb98a
lanceberge <bergeron.lance6@gmail.com>
2024-09-21 12:49:33 -0400

    add memoization

--- dynamic_programming/knapsack_with_memoization.cpp
+++ dynamic_programming/knapsack_with_memoization.cpp
@@ -12,31 +12,33 @@ class Knapsack {
 
 public:
   int knapsack(const vector<int> &weights, const vector<int> &profits, int C) {
-    return knapsackRecursive(weights, profits, C, 0);
+    vector<vector<int>> dp(profits.size(), vector<int>(C, -1));
+    return knapsackRecursive(dp, weights, profits, C, 0);
   }
 
 private:
   /*
-    Brute Force Solution:
     At every index, do a recursive call both excluding and including the
-    current index. Thus we get the profit of every subset
-    Time complexity: 2^n
+    current index. Thus we get the profit of every subset. We save
+    Time Complexity: N * C
   */
-  int knapsackRecursive(const vector<int> &weights, const vector<int> &profits,
-                        int C, int idx) {
+  int knapsackRecursive(vector<vector<int>> &dp, const vector<int> &weights,
+                        const vector<int> &profits, int C, int idx) {
     if (C <= 0 || idx >= weights.size()) {
       return 0;
     }
 
     int inclusiveProfit =
         weights[idx] <= C
-            ? profits[idx] +
-                  knapsackRecursive(profits, weights, C - weights[idx], idx + 1)
+            ? profits[idx] + knapsackRecursive(dp, profits, weights,
+                                               C - weights[idx], idx + 1)
             : 0;
 
-    int exclusiveProfit = knapsackRecursive(profits, weights, C, idx + 1);
+    int exclusiveProfit = knapsackRecursive(dp, profits, weights, C, idx + 1);
 
-    return max(inclusiveProfit, exclusiveProfit);
+    int currentMax = max(inclusiveProfit, exclusiveProfit);
+    dp[idx][C] = currentMax;
+    return currentMax;
   }
 };
 
