458cf6493f78ebc3b1b258b6f94da920ef785a9b
lanceberge <bergeron.lance6@gmail.com>
2024-09-22 13:05:18 -0400

    merge function

--- sorting/merge_sort.cpp
+++ sorting/merge_sort.cpp
@@ -11,16 +11,32 @@ class Merge {
 public:
   void mergeSort(vector<int> &arr) {
     // For each merge size
-    for (int mergeSize = 2; mergeSize < arr.size(); mergeSize *= 2) {
+    for (int mergeSize = 1; mergeSize < arr.size(); mergeSize *= 2) {
       // For each subarray
-      for (int i = 0; i < arr.size(); i += mergeSize) {
+      for (int i = 0; i < arr.size(); i += 2 * mergeSize) {
         merge(arr, i, mergeSize);
       }
     }
   }
 
 private:
-  void merge(vector<int> &arr, int i, int mergeSize) {}
+  void merge(vector<int> &arr, int startIdx, int mergeSize) {
+    int l_start = startIdx;
+    int r_start = startIdx + mergeSize;
+
+    int insertedIdx = startIdx;
+    for (int i = 0; i < mergeSize * 2; ++i) {
+      if (arr[l_start] <= arr[r_start]) {
+        arr[insertedIdx] = arr[l_start];
+        l_start += 1;
+      }
+
+      else {
+        arr[insertedIdx] = arr[r_start];
+        r_start += 1;
+      }
+    }
+  }
 };
 
 int main() {
