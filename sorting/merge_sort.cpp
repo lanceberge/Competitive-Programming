30a40dd6710394b5c3e2949a8664c1624e66edd1
lanceberge <bergeron.lance6@gmail.com>
2024-09-23 18:36:20 -0400

    recursive and bottom up merge sort

--- sorting/merge_sort.cpp
+++ sorting/merge_sort.cpp
@@ -3,39 +3,66 @@
 
 using namespace std;
 
-/*
-  Iterative, bottom-up merge sort
-*/
 class Merge {
 
 public:
   void mergeSort(vector<int> &arr) {
-    // For each merge size
-    for (int mergeSize = 1; mergeSize < arr.size(); mergeSize *= 2) {
-      // For each subarray
-      for (int i = 0; i < arr.size(); i += 2 * mergeSize) {
-        merge(arr, i, mergeSize);
-      }
-    }
+	mergeSortRecursive(arr, 0, arr.size() - 1);
   }
 
 private:
-  void merge(vector<int> &arr, int startIdx, int mergeSize) {
-    int l_start = startIdx;
-    int r_start = startIdx + mergeSize;
+  void mergeSortRecursive(vector<int> &arr, int l, int r) {
+	if (l >= r) {
+	  return;
+	}
 
-    int insertedIdx = startIdx;
-    for (int i = 0; i < mergeSize * 2; ++i) {
-      if (arr[l_start] <= arr[r_start]) {
-        arr[insertedIdx] = arr[l_start];
-        l_start += 1;
-      }
+	int mid = l + (r - l) / 2;
+	mergeSortRecursive(arr, l, mid);
+	mergeSortRecursive(arr, mid + 1, r);
+	merge(arr, l, r, mid);
+  }
 
-      else {
-        arr[insertedIdx] = arr[r_start];
-        r_start += 1;
-      }
-    }
+  void merge(vector<int> &arr, int l, int r, int mid) {
+	int n1 = mid - l + 1;
+	int n2 = r - mid;
+
+	// Create temp vectors
+	vector<int> L(n1), R(n2);
+	for (int i = 0; i < n1; i++) {
+	  L[i] = arr[l + i];
+	}
+
+	for (int j = 0; j < n2; j++) {
+	  R[j] = arr[mid + 1 + j];
+	}
+
+	int i = 0, j = 0;
+	int k = l;
+
+	// merge temp vectors
+	while (i < n1 && j < n2) {
+	  if (L[i] <= R[j]) {
+		arr[k] = L[i];
+		i++;
+	  } else {
+		arr[k] = R[j];
+		j++;
+	  }
+	  k++;
+	}
+
+	// Copy the remaining elements
+	while (i < n1) {
+	  arr[k] = L[i];
+	  i++;
+	  k++;
+	}
+
+	while (j < n2) {
+	  arr[k] = R[j];
+	  j++;
+	  k++;
+	}
   }
 };
 
@@ -45,7 +72,7 @@ int main() {
   merge.mergeSort(arr);
 
   for (auto x : arr) {
-    cout << x << " ";
+	cout << x << " ";
   }
   cout << "\n";
 
