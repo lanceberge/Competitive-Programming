c6d1f829fed285f164d120f04f5bc63574019c4a
lanceberge <bergeron.lance6@gmail.com>
2025-04-19 13:07:09 -0400

    validate bst

---
+++ BST/validate_bst.cpp
@@ -0,0 +1,36 @@
+#include <climits>
+
+class TreeNode {
+public:
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class ValidateBST {
+public:
+  bool valid(TreeNode *node, long left, long right) {
+    if (!node) {
+      return true;
+    }
+
+    if (node->val <= left || node->val >= right) {
+      return false;
+    }
+
+    return valid(node->left, left, node->val) &&
+           valid(node->right, node->val, right);
+  }
+
+  bool isValidBST(TreeNode *root) {
+    if (root == nullptr) {
+      return true;
+    }
+
+    return valid(root, LONG_MIN, LONG_MAX);
+  }
+};
